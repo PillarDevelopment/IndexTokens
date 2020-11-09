cf98299c1cfef0541ce91bd5602195749731408c
lastmohicans <lastmohicans@protonmail.com>
2020-11-09 22:42:29 +0300

    config for wBTCwETH

--- scripts/set-migrate-03.ts
+++ scripts/set-migrate-03.ts
@@ -32,30 +32,14 @@ const issuanceModule_address = String(process.env.ISSUANCEMODULE);
 const componentManageModule_address = String(process.env.COMPONENTMANAGEMODULE);
 const _components: Address[] = [
     
-    '0x61f0bb2271d491415390e2A902FA0eD5E5cd4E8B',
-    '0xbbb0a5Cc397c5f03348818e4181120abE41C6a87',
-    '0xe4B5A287b250B5B2efae95c2AacF7E841BA88073',
-    '0xBb4066a1da3e3F0b20c1C971A29d7298cbed719b',
-    '0x3AC7B98e643dbFD7D71B1D04e799A2bA505E584f',
-    '0x97906a83004e961FD4b28C1EB0193c0b1e972810',
-    '0xE13862A029FbAeD6278d3008C22856135c435940',
-    '0xF0f02Ed6857B541b6a7fc6d43B1C28548a1911fA',
-    '0xd6c53dc8F8F8B7778126D282C45A54B7a0Ef4797',
-    '0xd1807374B29cFF9E9925686A7289FbcB5341a55c',
-    '0xD43a4a7F3F930F6e6FC0844894cD3C26113538fF'
+    '0x577d296678535e4903d59a4c929b718e1d575e0a',
+    '0xe4319c0cd15fc7517fa8414a08b08b5f7bfc0794',
+    '0xc778417e063141139fce010982780140aa0cd5ab '
 ];
 const _units: BigNumberish[] = [
-    '1000000000000000000',
-    '2000000000000000000',
-    '3000000000000000000',
-    '4000000000000000000',
-    '5000000000000000000',
-    '6000000000000000000',
-    '7000000000000000000',
-    '8000000000000000000',
-    '9000000000000000000',
-    '10000000000000000000',
-    '11000000000000000000'
+    '25000000000000000000',
+    '25000000000000000000',
+    '50000000000000000000'
 ];
 
 async function main(): Promise<any> {
@@ -84,8 +68,8 @@ async function main(): Promise<any> {
         _units,
         [issuanceModule.address, componentManageModule.address],
         owner.address,
-        'Set 11 value tokens',
-        'Set11',
+        'Test WBTC_WETH Index',
+        'wBTCwETHIndex',
     );        
     //await timeout(120000);
     //console.log("setTokenCreator.create", "receipt.hash", receipt.hash);
